fdb80fbfc64624c0cdafc31cb6828a227a78b4d0
Elias McCoy <emccoy1700@gmail.com>
2019-02-07 19:19:35 -0800

    CSIS-250 spring 2019 assignment 1

--- CSIS-250-Homework_2/Sphere_Properties.py
+++ CSIS-250-Homework_2/Sphere_Properties.py
@@ -1,10 +1,23 @@
+"""
+Program to perform calculations of spherical geometry
+
+Program: Sphere.py
+Author: Elias McCoy
+Last date modified: 2/7/19
+
+When given a valid value for a radius, the purpose of this program is to compute the diameter, circumference,
+surface area, and volume of a sphere. It uses the absolute value of the input to ensure it can also handle negative
+numbers. It also use a try catch statement to handle unexpected inputs such as strings
+"""
+
 from math import pi
+
 while True:
     try:
-        radius = float(input("Enter the radius of a cirlce: "))
+        radius = abs(float(input("Enter the radius of a cirlce: ")))
         print("You entered " + str(radius))
     except ValueError:
-        print("Please enter numeric values only")
+        print("Please enter numeric values only!")
     else:
         break
 
