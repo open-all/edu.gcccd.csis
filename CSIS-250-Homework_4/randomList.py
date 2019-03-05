e7e6cde0d3a100c79a49e11ad4c224eb504e192f
Elias McCoy <emccoy1700@gmail.com>
2019-03-04 19:24:44 -0800

    text output stream formatting
    Changed the word "Average" in the output to "Avg" to meet design specs. 
    Changed LI to List to better reflect that it is a mutable data type, not a constant.

--- CSIS-250-Homework_4/randomList.py
+++ CSIS-250-Homework_4/randomList.py
@@ -1,5 +1,5 @@
 import random
-LI = [2]
+List = [2]
 for i in range(random.randint(4, 7)):
-    LI.append(random.randint(LI[i], LI[i]*2 - 1))
-print("List: " + " ".join(map(str, LI)) + "  - Sum: " + str(sum(LI)) + " - Median: " + (str(((LI[len(LI)//2] + LI[-(len(LI)+1)//2])/2)) if len(LI) % 2 == 0 else str(int(LI[len(LI)//2]))) + " - Average: " + "{0:.2f}".format(sum(LI)/len(LI)))
+    List.append(random.randint(List[i], List[i] * 2 - 1))
+print("List: " + " ".join(map(str, List)) + "  - Sum: " + str(sum(List)) + " - Median: " + (str(((List[len(List) // 2] + List[-(len(List) + 1) // 2]) / 2)) if len(List) % 2 == 0 else str(int(List[len(List) // 2]))) + " - Avg: " + "{0:.2f}".format(sum(List) / len(List)))
